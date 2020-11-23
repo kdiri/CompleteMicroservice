db10564484edb5eaa75b4255c47f42cecad94258
Kemal Diri <kemaldiri@gmail.com>
2020-11-23 09:24:37 +0100

    setup :star2:

---
+++ userService/testClient.js
@@ -0,0 +1,17 @@
+const messages = require('./proto/user_pb');
+const services = require('./proto/user_grpc_pb');
+const grpc = require('@grpc/grpc-js');
+
+function main() {
+    const client = new services.UserSvcClient('localhost:8080', grpc.credentials.createInsecure());
+
+    let registerReq = new messages.RegisterRequest();
+    registerReq.setName("Hello");
+    registerReq.setEmail("hello@world.com");
+    registerReq.setPassword("Password");
+    client.register(registerReq, function(err, response) {
+        console.log(response);
+    });
+}
+
+main();
